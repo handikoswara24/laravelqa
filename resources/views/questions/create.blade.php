c7730ba92f7b06446e0c072c2055d7453c711db4
handikoswara24 <39146228+handikoswara24@users.noreply.github.com>
2021-01-14 20:56:50 +0700

    Validate and saving question

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -21,7 +21,7 @@
                         @csrf
                         <div class="form-group">
                             <label for="question-title">Question Title</label>
-                            <input type="text" name="title" id="question-title"
+                            <input type="text" name="title" id="question-title" value="{{ old('title') }}"
                                 class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
 
                             @if($errors->has('title'))
@@ -33,7 +33,7 @@
                         <div class="form-group">
                             <label for="question-body">Explain your question</label>
                             <textarea name="body" id="question-body" rows="10"
-                                class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+                                class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ old('body') }}</textarea>
 
                             @if($errors->has('body'))
                             <div class="invalid-feedback">
