73cce7337a6ade72c74e3ecac1e2ba05a7cfde4b
handikoswara24 <39146228+handikoswara24@users.noreply.github.com>
2021-01-17 17:11:05 +0700

    update best answer id if deleted

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -34,12 +34,22 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer) {
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if ($answer->id == $question->best_answer_id) {
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
     }
 
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : "";
     }
 }
