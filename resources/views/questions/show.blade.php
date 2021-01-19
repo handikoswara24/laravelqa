a9502888992bf500c8074630243dc4105b67f1bc
handikoswara24 <39146228+handikoswara24@users.noreply.github.com>
2021-01-19 19:00:24 +0700

    Favorite button

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -30,10 +30,19 @@
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
                             <a title="Click to mark as favorite question (Click again to undo)"
-                                class="favorite mt-2 favorited">
+                                class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
+                                onclick="event.preventDefault();document.getElementById('favorite-question-{{$question->id}}').submit()">
                                 <i class="fas fa-star fa-2x"></i>
-                                <span class="favorite-count">123</span>
+                                <span class="favorite-count">{{$question->favorites_count}}</span>
                             </a>
+
+                            <form id="favorite-question-{{$question->id}}" method="POST"
+                                action="/questions/{{ $question->id }}/favorites" class="d-none">
+                                @csrf
+                                @if ($question->is_favorited)
+                                @method("DELETE")
+                                @endif
+                            </form>
                         </div>
                         <div class="media-body">
                             {!! $question->body_html !!}
