2745cd4e7cbb8520cfc5fee9bd28cccb35b8e9aa
handikoswara24 <39146228+handikoswara24@users.noreply.github.com>
2021-01-26 19:58:18 +0700

    implement DRY principle

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Str;
 class Question extends Model
 {
     use HasFactory;
+    use VotableTrait;
+
     protected $fillable = ["title", "body"];
 
     public function user()
@@ -79,20 +81,5 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
-    }
-
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
     }
 }
