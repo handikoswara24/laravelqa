4dbbbcf22220d23e8260872c182aecbc8ed956f0
handikoswara24 <39146228+handikoswara24@users.noreply.github.com>
2021-01-30 17:20:06 +0700

    Add izitoast

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -29,24 +29,52 @@ export default {
           (res) => {
             this.bodyHtml = res.data.body_html;
             this.editing = false;
-            alert(res.data.message);
+            this.$toast.success(res.data.message, "Success", { timeout: 3000 });
           },
           (err) => {
-            alert(err.response.data.message);
+            this.$toast.error(err.response.data.message, "Error", {
+              timeout: 3000,
+            });
           }
         );
     },
     destroy() {
-      if (confirm("Are you sure?")) {
-        axios.delete(this.endPoint).then(
-          (res) => {
-            $(this.$el).fadeOut(500, () => {
-              alert(res.data.message);
-            });
-          },
-          (err) => {}
-        );
-      }
+      this.$toast.question("Are you sure about that?", "Confirm", {
+        timeout: 20000,
+        close: false,
+        overlay: true,
+        displayMode: "once",
+        id: "question",
+        zindex: 999,
+        title: "Hey",
+        message: "Are you sure about that?",
+        position: "center",
+        buttons: [
+          [
+            "<button><b>YES</b></button>",
+            (instance, toast) => {
+              axios.delete(this.endPoint).then(
+                (res) => {
+                  $(this.$el).fadeOut(500, () => {
+                    this.$toast.success(res.data.message, "Success", {
+                      timeout: 3000,
+                    });
+                  });
+                },
+                (err) => {}
+              );
+              instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+            },
+            true,
+          ],
+          [
+            "<button>NO</button>",
+            function (instance, toast) {
+              instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+            },
+          ],
+        ],
+      });
     },
   },
   computed: {
