e9513b8f0d6c883ab93d8feb38aea9c4d66e5a1b
handikoswara24 <39146228+handikoswara24@users.noreply.github.com>
2021-01-31 17:08:08 +0700

    Fix issue accept

--- resources/js/components/Accept.vue
+++ resources/js/components/Accept.vue
@@ -19,6 +19,8 @@
 </template>
 
 <script>
+import eventBus from "../event-bus.js";
+import EventBus from "../event-bus.js";
 export default {
   props: ["answer"],
   data() {
@@ -26,6 +28,11 @@ export default {
       isBest: this.answer.is_best,
       id: this.answer.id,
     };
+  },
+  created() {
+    EventBus.$on("accepted", (id) => {
+      this.isBest = id == this.id;
+    });
   },
   computed: {
     canAccept() {
@@ -47,6 +54,8 @@ export default {
             position: "bottomLeft",
           });
           this.isBest = true;
+
+          EventBus.$emit("accepted", this.id);
         },
         (err) => {}
       );
